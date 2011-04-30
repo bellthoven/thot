7203382507719c78a5a1ce4a47e0d0e78a92d342
Gustavo Dutra <gustavotkg@gmail.com>
2011-04-30 19:04:00 -0300

    plugin manager and event handler working

---
+++ thot/plugins/__init__.py
@@ -0,0 +1,50 @@
+from pkg_resources import iter_entry_points
+
+class ThotPlugin(object):
+
+	def name(self): abstract
+	
+	def description(self): abstract
+	
+	def bootstrap(self): pass
+
+	def on_before_parse_args(self, optgroup): pass
+
+	def on_after_parse_args(self, options): return dict()
+	
+	def run(self, options): pass
+	
+	def __str__(self):
+		return self.name()
+
+class PluginManager(object):
+	_loaded = []
+	_builtins = (
+		('thot.plugins.project', 'ThotProject'),
+	)
+
+	def _load_builtin_plugins(self):
+		for module,class_name in self._builtins:
+			plugin_module = __import__(module, globals(), locals(), [class_name])
+			plugin = getattr(plugin_module, class_name)() # Initialize!
+			self.add_plugin( plugin )
+			globals()[class_name] = plugin
+
+	def load_plugins(self):
+		self._load_builtin_plugins()
+
+		for plugin in iter_entry_points(group='thot.plugins'):
+			loaded_plugin = plugin.load()
+			try:
+				plugin = loaded_plugin() # instanciate plugin's class
+				self.add_plugin( plugin )
+				return True
+			except:
+				return False
+
+	def add_plugin(self, plugin):
+		if isinstance(plugin, ThotPlugin) and plugin not in self._loaded:
+			self._loaded.append( plugin )
+
+	def plugins(self):
+		return iter(self._loaded)
